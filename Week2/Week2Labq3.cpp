9910ae879ff846980baaa6df3e9ad67a6657d81f
AnjaliNegi01 <68815555+AnjaliNegi01@users.noreply.github.com>
2021-11-13 12:50:13 +0530

    Problem 3

--- Week2/Week2Labq3.cpp
+++ Week2/Week2Labq3.cpp
@@ -1,19 +1,14 @@
 #include<iostream>
-#include<fstream>
 using namespace std;
 int main(){
-    ifstream in;
-    in.open("input.txt");
-    ofstream out;
-    out.open("output.txt");
     int n,t,k;
     int arr[100];
-    in >> t;
+    cin >> t;
     while(t){
-        in >> n;
+        cin >> n;
         for(int i=0;i<n;i++)
-            in >> arr[i];
-        in >> k;
+            cin >> arr[i];
+        cin >> k;
         int count =1;
         for(int i=0;i<n;i++){
             for(int j=i;j<n-i-1;j++){
@@ -22,7 +17,7 @@ int main(){
                 }
             }
         }
-        out << count << endl;
+        cout << count << endl;
 
     t--;
     }
